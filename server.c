72fd389383db964cbc77e85a590e884d40d81f7d
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2024-09-06 23:20:39 +0530

    Feat: socket creation refactored

--- server.c
+++ server.c
@@ -4,31 +4,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <winnt.h>
+#include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main() {
+int create_socket(int port) {
   WSADATA wsa;
-  int socket_descriptor, client_socket, client_size;
-  struct sockaddr_in server_addr, client_addr;
-  char server_buffer[1000], client_buffer[1000];
+  int socket_descriptor;
+  struct sockaddr_in server_addr;
 
-  memset(server_buffer, '\0', sizeof(server_buffer));
-  memset(client_buffer, '\0', sizeof(client_buffer));
-
-  strcpy(server_buffer, "HTTP/1.1 200 OK\r\n"
-                        "Connection: Close\r\n"
-                        "Content-Type: text/html\r\n"
-                        "Content-Length: 48\r\n"
-                        "Date: Wed, 20 Jul 2016 10:55:30 GMT\r\n"
-                        "Last-Modified: Tue, 19 Jul 2016 00:59:33 GMT\r\n"
-                        "Server: Apache\r\n"
-                        "\r\n"
-                        "<html><body><h1>Hello World!</h1></body></html>");
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  InetPton(AF_INET, __TEXT("127.0.0.1"), &server_addr.sin_addr.s_addr);
 
   if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
     printf("Cant init wsa");
-    return 0;
+    return -1;
   }
 
   socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,12 +27,8 @@ int main() {
     printf("Cant create a socket");
     closesocket(socket_descriptor);
     WSACleanup();
-    return 0;
+    return -1;
   }
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(3000);
-  InetPton(AF_INET, __TEXT("127.0.0.1"), &server_addr.sin_addr.s_addr);
 
   if (bind(socket_descriptor, (struct sockaddr *)&server_addr,
            sizeof(server_addr))) {
@@ -50,6 +37,26 @@ int main() {
     WSACleanup();
     return 0;
   }
+
+  return socket_descriptor;
+}
+
+int main() {
+  int socket_descriptor = create_socket(3000);
+
+  int client_socket, client_size;
+  struct sockaddr_in client_addr;
+  char server_buffer[1000], client_buffer[1000];
+
+  memset(server_buffer, '\0', sizeof(server_buffer));
+  memset(client_buffer, '\0', sizeof(client_buffer));
+
+  strcpy(server_buffer, "HTTP/1.1 200 OK\r\n"
+                        "Connection: close\r\n"
+                        "Content-Type: text/html\r\n"
+                        "Content-Length: 48\r\n"
+                        "\r\n"
+                        "<html><body><h1>Hello World!</h1></body></html>");
 
   if (listen(socket_descriptor, 1) < 0) {
     printf("Error while listening");
@@ -65,21 +72,21 @@ int main() {
 
     if (client_socket < 0) {
       printf("Cant accept");
-      closesocket(socket_descriptor);
+      closesocket(client_socket);
       WSACleanup();
       return 0;
     }
 
     if (recv(client_socket, client_buffer, strlen(client_buffer), 0) < 0) {
       printf("Cant recieve");
-      closesocket(socket_descriptor);
+      closesocket(client_socket);
       WSACleanup();
       return 0;
     }
 
     if (send(client_socket, server_buffer, strlen(server_buffer), 0) < 0) {
       printf("Cant send");
-      closesocket(socket_descriptor);
+      closesocket(client_socket);
       WSACleanup();
       return 0;
     }
